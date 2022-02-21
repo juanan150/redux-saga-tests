664abed6fa32fc2c97402b6b59143486167fe722
juanan150 <juanan0309@gmail.com>
2022-02-21 17:55:25 -0500

    Delete app comments

--- base-app/client/src/App.tsx
+++ base-app/client/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes } from './app/components/nav/Routes'
 import { useGlobalToast } from './features/toast/hooks/useGlobalToast'
 
 export function App(): React.ReactElement {
-  //test
   useGlobalToast()
   return (
     <>
